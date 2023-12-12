542734a59428b8a8f368e040bb63200063294403
JJ <jonjones320@gmail.com>
2023-12-11 19:45:33 -0800

    added arrow.js

---
+++ arrow.js
@@ -0,0 +1,28 @@
+// ES5 Version: 
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+//   Refactor with ES2015 arrow functions
+
+
+const double = arr => arr.map(val => val * 2);
+
+////////////////////////////////////////////////////////////////
+
+Refactor:
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
